8414d33e07260bc84f88dce27153782b4bf6e401
JD Villarroel <jdvillarroel@gmail.com>
2021-04-12 00:52:14 -0500

    From Rosi Laptop.

---
+++ public/js/fb.js
@@ -0,0 +1,181 @@
+// *************** Functions ********************* //
+// Create user in firebase using email and password. Returns new user created.
+const createUser = async (userInfo) => {
+    const userCredential = await auth.createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+    const userAccount = await usersRef.doc(userCredential.user.uid).set({
+        firstName: userInfo.fName,
+        lastName: userInfo.lName,
+        email: userInfo.email,
+        createdAt: new Date()
+    });
+
+    return userCredential.user;
+}
+
+// Create account to user.
+const createAccount = async (user) => {
+    const account = await accountsRef.doc(user.uid).set({
+        balance: 0
+    });
+
+    return account;
+}
+
+// Get user account.
+const getAccount = async (user) => {
+    const account = await accountsRef.doc(user.uid).get();
+
+    return account;
+}
+
+// Withdraw from account.
+const withdrawFromAccount = async (amount) => {
+    return accountsRef.doc().update({
+
+    })
+}
+
+// *********** Functions *************** //
+const handleSignUpModalClose = () => {
+    signUpForm.reset();
+    signUpModal.hide();
+};
+
+const handleSignInModalClose = () => {
+    signInForm.reset();
+    signInModal.hide();
+};
+
+
+// ************* Events Listeners *************** //
+
+// Show sign up modal form.
+signUp.addEventListener("click", (e) => {
+    e.preventDefault();
+    signUpModal.show();
+});
+
+// Show sign in modal form.
+signIn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signInModal.show();
+});
+
+// Sign up form handle.
+signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const userInfo = {
+        fName: signUpForm.fName.value,
+        lName: signUpForm.lName.value,
+        email: signUpForm.email.value.toLowerCase(),
+        password: signUpForm.password.value
+    }
+
+    if (userInfo.fName.length === 0 || userInfo.lName.length === 0 || userInfo.email.length === 0 || userInfo.password.length === 0) {
+        console.log("Can't proceed. Check your input data.");
+    } else {
+        createUser(userInfo)
+        .then((user) => {
+            return createAccount(user);            
+        })
+        .then(() => {
+            handleSignUpModalClose();
+        })
+        .catch(err => console.log(err.message));
+    }
+    
+});
+
+// Send money form
+sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const amount = parseFloat(transactionForm.amount.value);
+    const to = transactionForm.email.value.toLowerCase();
+    const description = transactionForm.description.value;
+    let user = auth.currentUser;
+    let userID = user.uid;
+    let balance = userAccount.data().balance;
+
+    if (amount <= balance) {
+        db.collection("accounts").doc(userID).update({
+        balance: firebase.firestore.FieldValue.increment(-amount)
+        // balance: parseFloat((balance - amount).toFixed(2)),
+        // transactions: [...transactions, {
+        //     to: to,
+        //     amount: amount,
+        //     date: new Date(),
+        //     description: description
+        // }]
+        })
+        .then(() => {
+            return db.collection("transactions").add({
+                from: user.email,
+                to: to,
+                amount: parseFloat(amount.toFixed(2)),
+                date: new Date(),
+                description: description,
+                status: "pending"
+            })
+        })
+        .then(console.log("Money Sent!"))
+        .catch(err => console.log(err.message));
+    } else {
+        console.log("Not enough funds to do this transaction.");
+    }
+    
+    transactionForm.reset();
+    
+});
+
+// Sign up modal form close.
+signUpBtnClose.addEventListener("click", handleSignUpModalClose);
+
+// Sign out user.
+signOut.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// Handle sign in user.
+signInBtn.addEventListener("click", (e)=> {
+    const email = signInForm.email.value;
+    const password = signInForm.password.value;
+
+    auth.signInWithEmailAndPassword(email.toLowerCase(), password)
+    .then((user) => {
+        // setupUser(user);
+        handleSignInModalClose();
+    })
+    .catch(err => {
+        console.log(err.code, err.message);
+    });
+});
+
+// Sign in modal form close.
+signInBtnClose.addEventListener("click", handleSignInModalClose);
+
+
+// ************ Auth Realtime Listener ********* //
+auth.onAuthStateChanged((user) => {
+
+    if (user) {
+        // Get user's account.
+        getAccount(user)
+        .then(account => {
+            userAccount = account;
+        });
+
+        // Real time listener.
+        accountsRef.doc(user.uid).onSnapshot((account) => {
+            userAccount = account;
+            setupUI(user, account.data());
+        });
+        
+        console.log("User logged in");
+    } else {
+        setupUI(null, null);
+        console.log("User logged out");
+    }
+});
---
+++ public/js/index.js
@@ -0,0 +1,68 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // ************ Firebase References ********* //
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const accountsRef = db.collection("accounts");
+const usersRef = db.collection("users");
+
+// Upadte firestore settings
+db.settings({ timestampsInSnapshots: true });
+
+
+const signUp = document.querySelector("#signup");
+const signIn = document.querySelector("#signin");
+const signOut = document.querySelector("#signout");
+
+const loggedUser = document.querySelector("#loggedUser");
+const regContent = document.querySelector("#reg-content");
+
+// ************* Modals ***************** //
+const signUpModal = new bootstrap.Modal(document.querySelector("#signup-modal"), {keyboard: true});
+const signUpBtn = document.querySelector("#signup-btn");
+const signUpBtnClose = document.querySelector("#signup-btn-close");
+const signInModal = new bootstrap.Modal(document.querySelector("#signin-modal"), {keyboard: true});
+const signInBtn = document.querySelector("#signin-btn");
+const signInBtnClose = document.querySelector("#signin-btn-close");
+
+// ************ Forms ******************* //
+const signUpForm = document.querySelector("#signup-form");
+const signInForm = document.querySelector("#signin-form");
+const transactionForm = document.querySelector("#transaction-form");
+const sendBtn = document.querySelector("#send-btn");
+
+// ************ Balance Card **************** //
+const balanceAvailable = document.querySelector("#balance-available");
+
+// ************ User account ****************** //
+let userAccount = null;
+
+// ***************** Functions ********************* //
+
+const setupUI = (user, account) => {
+    if (user) {
+
+        regContent.style.display = "block";
+        loggedUser.textContent = `Usuario: ${user.email}`;
+        balanceAvailable.textContent = `$ ${account.balance}`;
+
+    } else {
+        loggedUser.textContent = "";
+        regContent.style.display = "none";
+        balanceAvailable.textContent = "";
+    }
+};
+
